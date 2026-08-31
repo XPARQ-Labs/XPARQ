296e96cdcff01aecdb76052d90e5fce1e3d5af03
XPARQ-Labs <pawgnus@gmail.com>
2026-08-31 07:14:30 -0400

    remove bitcoin, ethereum, solana lib.rs

--- asset/src/lib.rs
+++ asset/src/lib.rs
@@ -4,10 +4,3 @@ mod program;
 
 pub use error::AssetIdParseError;
 pub use program::*;
-
-#[path = "bitcoin/bitcoin.rs"]
-pub mod bitcoin;
-#[path = "ethereum/ethereum.rs"]
-pub mod ethereum;
-#[path = "solana/solana.rs"]
-pub mod solana;
